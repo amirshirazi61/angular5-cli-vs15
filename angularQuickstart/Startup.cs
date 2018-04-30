fb80decc46d3479e1bef3195378349757f32c512
Amir Shirazi <ashirazi@quickbridge.com>
2018-04-30 08:57:27 -0700

    initial commit.

---
+++ angularQuickstart/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(angularQuickstart.Startup))]
+namespace angularQuickstart
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
